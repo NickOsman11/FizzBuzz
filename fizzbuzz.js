f6c384f089a41f21abca18f92d7e57bd61d97f65
Nick Osman <Nick.Osman@softwire.com>
2022-08-08 15:34:53 +0100

    prints numbers 1-100, but fizz if multiple of 3, buzz if multiple of 5, fizzbuzz if multiple of both

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,16 @@
 function fizzbuzz(){
 
     for (let i = 1; i<101; i++){
-        if (i % 3 == 0){
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log("fizzbuzz")
+        }
+        else if (i % 3 == 0){
             console.log("fizz")
         }
-        else{
+        else if (i % 5 == 0){
+            console.log("buzz")
+        }
+        else {
             console.log(i)
         }
     }
