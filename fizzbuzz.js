f756a4437e59f17050788e7a49880fb429e495fd
Nick Osman <Nick.Osman@softwire.com>
2022-08-09 11:02:50 +0100

    changed multiple of 13 operation to a function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,8 +7,9 @@ function ReverseFizzBuzzOrder(string){
 
 function AddFezzBeforeB(string){
 
-    var splitString = string.split(/z(?=B)/)
-    console.log(splitString)
+    var indexOfFirstB = string.indexOf("B")
+    return string.slice(0, indexOfFirstB) 
+                + "Fezz" + string.slice(indexOfFirstB)
 }
 
 function fizzbuzz(){
@@ -36,23 +37,12 @@ function fizzbuzz(){
 
         if (i % 13 == 0){
             
-            // if (outputString.includes("B")){
-            //     outputString = AddFezzBeforeB(outputString)
-            // }
-            // else{
-            //     outputString += "fezz"
-            // }
-
-        
-            var indexOfFirstB = outputString.indexOf("B")
-            if (indexOfFirstB != -1){
-                outputString = outputString.slice(0, indexOfFirstB) 
-                + "Fezz" + outputString.slice(indexOfFirstB)
+            if (outputString.includes("B")){
+                outputString = AddFezzBeforeB(outputString)
             }
             else{
-                outputString += "Fezz"
+                outputString += "fezz"
             }
-            
         }
 
         if (i % 17 == 0 && outputString != ""){
